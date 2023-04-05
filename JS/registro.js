c7435bf8832cface3cc0789e83416f35c9bf195d
robinson-arpi <robinson.arpi@gmail.com>
2023-04-04 22:23:35 -0500

    cambio en estilos

---
+++ JS/registro.js
@@ -0,0 +1,10 @@
+const inputFecha = document.getElementById('fecha');
+
+inputFecha.addEventListener('change', (evento) => {
+  const fecha = new Date(evento.target.value);
+  const dia = fecha.getDate().toString().padStart(2, '0');
+  const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+  const anio = fecha.getFullYear().toString();
+
+  inputFecha.value = `${dia}/${mes}/${anio}`;
+});
