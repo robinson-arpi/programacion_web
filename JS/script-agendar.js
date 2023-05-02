1c2ba1fbcc8c6ca1f8b727d65a0ed75e41e53365
KevinJuela20 <kevin.juelac@ucuenca.edu.ec>
2023-05-02 15:36:20 -0500

    Nuevos Cambios en agendar y localStorage en Agendar

---
+++ JS/script-agendar.js
@@ -0,0 +1,47 @@
+//Local storage para agendar
+
+// Obtener referencias a los campos de texto
+var inputFecha = document.querySelector('.fecha');
+var inputHora = document.querySelector('.hora');
+var inputDireccion = document.querySelector('.direccion');
+var buttonCancelar = document.querySelector('.boton-cancelar');
+var buttonAgendar = document.querySelector('.boton-agendar');
+
+// Obtener el contenido guardado del almacenamiento local
+var fechaGuardado = localStorage.getItem('fecha_agendar');
+var horaGuardada = localStorage.getItem('hora_agendar');
+var direccionGuardada = localStorage.getItem('direccion_agendar');
+
+// Restaurar el contenido si está disponible
+if (fechaGuardado != null)
+  inputFecha.value = fechaGuardado;
+  
+if (horaGuardada != null)
+  inputHora.value = horaGuardada;
+
+if (direccionGuardada != null)
+  inputDireccion.value = direccionGuardada;
+
+// Guardar el contenido cuando se cambie
+inputFecha.addEventListener('input', function() {
+  localStorage.setItem('fecha_agendar', inputFecha.value);
+});
+
+inputHora.addEventListener('input', function() {
+  localStorage.setItem('hora_agendar', inputHora.value);
+});
+
+inputDireccion.addEventListener('input', function() {
+    localStorage.setItem('direccion_agendar', inputDireccion.value);
+  });
+
+buttonCancelar.addEventListener('input',borrarCampos());
+buttonAgendar.addEventListener('input',borrarCampos())
+
+
+//Función que limpia campos al pulsar un botón u otro
+function borrarCampos(){
+    localStorage.setItem('fecha_agendar', '');
+    localStorage.setItem('hora_agendar', '');
+    localStorage.setItem('direccion_agendar', '');
+}
