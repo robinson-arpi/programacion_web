496e7439c3ec42cb9da9cf40a7960cee11a53235
Mauro Mizhquiri <rolando.mizhquiri@gmail.com>
2023-05-03 11:51:21 -0500

    agrego funcionalidad de descripcion de servicios

--- JS/descripcion_servicio.js
+++ JS/descripcion_servicio.js
@@ -1,18 +1,7 @@
-const todasLasImagenes = document.querySelectorAll('.contenedor-flotante div img')
-const imagenContenedor = document.querySelector('.contenedor-imagen')
-
-window.addEventListener('DOMContentLoaded',()=>{
-    todasLasImagenes[0].parentElement.classList.add('active');   
-})
-todasLasImagenes.forEach((image) =>{
-    image.addEventListener('mouseover',() =>{
-        imagenContenedor.querySelector('img').src = image.src;
-        resetearActivadorImagenes();
-        image.parentElement.classList.add('active');
-    })
-})
-function resetearActivadorImagenes(){
-    todasLasImagenes.forEach((img) =>{
-        img.parentElement.classList.remove('active')
-    })
+// para el botón cancelar y regresar a inicio
+function cancelar(event) {
+    window.location.href = "../HTML/servicios.html";
+}
+function agendarServicio(event) {
+    window.location.href = "../HTML/agendar-servicio.html";
 }
