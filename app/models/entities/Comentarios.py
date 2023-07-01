09ce21b0d2c8fc0cf7854ffd80236df5d679f640
KevinJuela20 <kevin.juelac@ucuenca.edu.ec>
2023-07-01 12:02:48 -0500

    titulos en mis paginas

---
+++ app/models/entities/Comentarios.py
@@ -0,0 +1,20 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+from sqlalchemy import Column, Integer, String, Boolean, Date
+from app import db
+
+
+class Comentarios(db.Model, UserMixin):
+    __tablename__ = 'comentarios'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    calificacion = Column(String(150), nullable=False)
+    categoria = Column(String(150), nullable=False)
+    mensaje = Column(String(350), nullable=False)
+
+    def __init__(self, id, nombre, calificacion, categoria, mensaje):
+        self.id = id
+        self.nombre = nombre
+        self.calificacion = calificacion
+        self.categoria = categoria
+        self.mensaje = mensaje
