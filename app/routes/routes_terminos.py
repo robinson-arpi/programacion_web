0312ac246395adbaab134db94be9719aa53e791a
KevinJuela20 <kevin.juelac@ucuenca.edu.ec>
2023-07-12 15:02:13 -0500

    Agregación de current_user

--- app/routes/routes_terminos.py
+++ app/routes/routes_terminos.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask_login import current_user
 from flask import Blueprint, render_template
 
 terminos_blueprint = Blueprint('terminos', __name__)
@@ -6,4 +6,4 @@ terminos_blueprint = Blueprint('terminos', __name__)
 # Ruta de terminos servicio
 @terminos_blueprint.route('/terminos_condiciones')
 def terminos_route():
-    return render_template('page_statics/terminos-condiciones.html')
+    return render_template('page_statics/terminos-condiciones.html', usuario = current_user)
